2e85b9788cadf34a59feecaf4258f2d781006790
Juan Brusco <juanbrusco_23@hotmail.com>
2016-11-25 11:48:48 -0300

    Tab premios

--- www/js/controllers/premios.controller.js
+++ www/js/controllers/premios.controller.js
@@ -1,6 +1,25 @@
 angular.module('premios.controller', [])
-  .controller('PremiosCtrl', function($scope) {
+  .controller('PremiosCtrl', function($scope, $ionicPopup, Premios) {
     $scope.settings = {
       enableFriends: true
+    };
+
+    $scope.premios = Premios.allPremios();
+
+    // A confirm dialog
+    $scope.showConfirm = function(premioId) {
+      var premio = Premios.getPremio(premioId);
+      var confirmPopup = $ionicPopup.confirm({
+        title:'Canjear',
+        template: premio.desc
+      });
+
+      confirmPopup.then(function(res) {
+        if(res) {
+          console.log('You are sure');
+        } else {
+          console.log('You are not sure');
+        }
+      });
     };
   });
--- www/js/factory/services.js
+++ www/js/factory/services.js
@@ -1,50 +1,124 @@
 angular.module('starter.services', [])
 
-.factory('Descuentos', function() {
+  .factory('Descuentos', function() {
 
-  // Un ejemplo de obtener datos hardcodeado
-  // Agregar servicios rest
-  var descuentos = [{
-    id: 0,
-    name: 'Descuento 1',
-    lastText: 'me descontas algo?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Descuento 2',
-    lastText: 'Quiero todo gratis',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Descuento 3',
-    lastText: 'Cargame creditos',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Descuento 4',
-    lastText: 'Android!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Descuento 5',
-    lastText: 'Vaikings',
-    face: 'img/mike.png'
-  }];
+    // Un ejemplo de obtener datos hardcodeado
+    // Agregar servicios rest
+    var descuentos = [{
+      id: 0,
+      name: 'Descuento 1',
+      lastText: 'me descontas algo?',
+      face: 'img/ionic.png'
+    }, {
+      id: 1,
+      name: 'Descuento 2',
+      lastText: 'Quiero todo gratis',
+      face: 'img/ionic.png'
+    }, {
+      id: 2,
+      name: 'Descuento 3',
+      lastText: 'Cargame creditos',
+      face: 'img/ionic.png'
+    }, {
+      id: 3,
+      name: 'Descuento 4',
+      lastText: 'Android!',
+      face: 'img/ionic.png'
+    }, {
+      id: 4,
+      name: 'Descuento 5',
+      lastText: 'Vaikings',
+      face: 'img/ionic.png'
+    }];
 
-  return {
-    all: function() {
-      return descuentos;
-    },
-    remove: function(descuento) {
-      descuentos.splice(descuentos.indexOf(descuento), 1);
-    },
-    get: function(descuentoId) {
-      for (var i = 0; i < descuentos.length; i++) {
-        if (descuentos[i].id === parseInt(descuentoId)) {
-          return descuentos[i];
+    return {
+      all: function() {
+        return descuentos;
+      },
+      remove: function(descuento) {
+        descuentos.splice(descuentos.indexOf(descuento), 1);
+      },
+      get: function(descuentoId) {
+        for (var i = 0; i < descuentos.length; i++) {
+          if (descuentos[i].id === parseInt(descuentoId)) {
+            return descuentos[i];
+          }
         }
+        return null;
       }
-      return null;
-    }
-  };
-});
+    };
+  })
+
+  .factory('Premios', function() {
+    // Might use a resource here that returns a JSON array
+
+    // https://www.uno-de-piera.com/consumir-json-con-angularjs-con-resources-y-http/
+
+    // Some fake testing data
+    var premios = [
+      {
+        id: 0,
+        title: 'La Granja Chocolatada',
+        desc: 'La Granja Chocolatada. Diversión asegurada para lo más chicos',
+        img: 'http://resources.personal.com.ar/images/test/Club-Detalle-Granja.png',
+        puntos: '999999',
+        pesos: '99.999,99'
+      },
+      {
+        id: 1,
+        title: 'Vuelo en globo',
+        desc: 'Experiencia vuelo en globo en los cielos de Capilla del Señor',
+        img: 'http://resources.personal.com.ar/images/test/BsAs_Outdoor_globo-1.png',
+        puntos: '999999',
+        pesos: '99.999,99'
+      },
+      {
+        id: 2,
+        title: 'Pack Internet',
+        desc: 'Pack Internet + 7000 SMS x 7 dias',
+        img: 'http://resources.personal.com.ar/images/test/Pack-1.png',
+        puntos: '999999',
+        pesos: '99.999,99'
+      },
+      {
+        id: 0,
+        title: 'La Granja Chocolatada',
+        desc: 'La Granja Chocolatada. Diversión asegurada para lo más chicos',
+        img: 'http://resources.personal.com.ar/images/test/Club-Detalle-Granja.png',
+        puntos: '999999',
+        pesos: '99.999,99'
+      },
+      {
+        id: 1,
+        title: 'Vuelo en globo',
+        desc: 'Experiencia vuelo en globo en los cielos de Capilla del Señor',
+        img: 'http://resources.personal.com.ar/images/test/BsAs_Outdoor_globo-1.png',
+        puntos: '999999',
+        pesos: '99.999,99'
+      },
+      {
+        id: 2,
+        title: 'Pack Internet',
+        desc: 'Pack Internet + 7000 SMS x 7 dias',
+        img: 'http://resources.personal.com.ar/images/test/Pack-1.png',
+        puntos: '999999',
+        pesos: '99.999,99'
+      },
+
+    ];
+
+    return {
+      allPremios: function() {
+        return premios;
+      },
+      getPremio: function(premioId) {
+        for (var i = 0; i < premios.length; i++) {
+          if (premios[i].id === parseInt(premioId)) {
+            return premios[i];
+          }
+        }
+        return null;
+      }
+    };
+  });
+
