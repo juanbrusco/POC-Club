e8d612a1a2ccca73357d5dbff201dc664153194c
Juan Brusco <juanbrusco_23@hotmail.com>
2016-11-29 12:43:12 -0300

    Styles generales + nueva lista de premios

--- www/js/controllers/premios.controller.js
+++ www/js/controllers/premios.controller.js
@@ -6,6 +6,33 @@ angular.module('premios.controller', [])
 
     $scope.premios = Premios.allPremios();
 
+    // PopUp custom
+    $scope.showPopup = function(premioId) {
+      var premio = Premios.getPremio(premioId);
+      $scope.data = {};
+
+      // An elaborate, custom popup
+      var myPopup = $ionicPopup.show({
+        template: '<img ng-src="http://resources.personal.com.ar/images/test/Logo-Sushi.png" style="width: 60px; height: 40px;"/>',
+        title: 'Canjear Premio',
+        subTitle: premio.puntos + ' Puntos + $' + premio.pesos,
+        scope: $scope,
+        buttons: [
+          { text: 'Cancelar',
+            type: 'button-light'},
+          {
+            text: '<b>Canjear</b>',
+            type: 'button-positive',
+            onTap: function(e) {
+
+            }
+          }
+        ]
+      });
+
+
+    };
+
     // A confirm dialog
     $scope.showConfirm = function(premioId) {
       var premio = Premios.getPremio(premioId);
