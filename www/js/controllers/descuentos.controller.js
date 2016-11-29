d62eb764891eb91873208ed6ebba5bd0a1f6a671
marcos.dip <marcosjdip@gmail.com>
2016-11-29 16:32:06 -0300

    Tab descuentos

--- www/js/controllers/descuentos.controller.js
+++ www/js/controllers/descuentos.controller.js
@@ -1,7 +1,39 @@
 angular.module('descuentos.controller', [])
-  .controller('DescuentosCtrl', function($scope, Descuentos) {
-    $scope.descuentos = Descuentos.all();
-    $scope.remove = function(descuento) {
-      Descuentos.remove(descuento);
+  .controller('DescuentosCtrl', function($scope,$ionicPopup,Descuentos) {
+
+    Descuentos.all().then(function(data){
+      $scope.descuentos = data
+    });
+
+    $scope.loadMore =function() {
+      Descuentos.loadMore().then(function(data){
+        [].push.apply($scope.descuentos, data);
+        Descuentos.incrementCount();
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
     };
+
+    $scope.$on('$stateChangeSuccess', function() {
+      $scope.loadMore();
+    });
+
+
+
+    // A confirm dialog
+    $scope.showConfirm = function(descuentoId) {
+      var descuento = Descuentos.getDescuento(descuentoId);
+      var confirmPopup = $ionicPopup.confirm({
+        title:'Canjear',
+        template: descuento.name
+      });
+
+      confirmPopup.then(function(res) {
+        if(res) {
+          console.log('You are sure');
+        } else {
+          console.log('You are not sure');
+        }
+      });
+    };
+
   });
--- www/js/factory/services.js
+++ www/js/factory/services.js
@@ -1,50 +1,39 @@
 angular.module('starter.services', [])
 
-  .factory('Descuentos', function() {
-
-    // Un ejemplo de obtener datos hardcodeado
-    // Agregar servicios rest
-    var descuentos = [{
-      id: 0,
-      name: 'Descuento 1',
-      lastText: 'me descontas algo?',
-      face: 'img/ionic.png'
-    }, {
-      id: 1,
-      name: 'Descuento 2',
-      lastText: 'Quiero todo gratis',
-      face: 'img/ionic.png'
-    }, {
-      id: 2,
-      name: 'Descuento 3',
-      lastText: 'Cargame creditos',
-      face: 'img/ionic.png'
-    }, {
-      id: 3,
-      name: 'Descuento 4',
-      lastText: 'Android!',
-      face: 'img/ionic.png'
-    }, {
-      id: 4,
-      name: 'Descuento 5',
-      lastText: 'Vaikings',
-      face: 'img/ionic.png'
-    }];
-
+  .factory('Descuentos', function($http) {
+    var all, more = [];
+    var countF = 1;
+    var getData = function() {
+      return $http.get("https://club.personal.com.ar/club/services/catalog/benefits/distinguished")
+        .then(function(response) {
+          all = response.data;
+          return all;
+        });
+    }
+    var loadMore = function() {
+      return $http.get("https://club.personal.com.ar/club/services/catalog/items?",
+        {params:{$catalogItemType: 2, isForm: true, page: countF}})
+        .then(function(response) {
+          more = response.data;
+          return more;
+        });
+    }
     return {
-      all: function() {
-        return descuentos;
-      },
-      remove: function(descuento) {
-        descuentos.splice(descuentos.indexOf(descuento), 1);
-      },
-      get: function(descuentoId) {
-        for (var i = 0; i < descuentos.length; i++) {
-          if (descuentos[i].id === parseInt(descuentoId)) {
-            return descuentos[i];
+      all: getData,
+
+      getDescuento: function(descuentoId) {
+        for (var i = 0; i < all.length; i++) {
+          if (all[i].id === parseInt(descuentoId)) {
+            return all[i];
           }
         }
         return null;
+      },
+
+      loadMore: loadMore,
+
+      incrementCount:function(){
+        countF++;
       }
     };
   })
