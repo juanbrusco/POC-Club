b09da417b0d2f0b8dd5d30e17d30ab98a6a79525
Luis Phravichit <luis.phravichit@neoris.com>
2016-12-02 18:18:32 -0300

    Banner v.0.3

--- www/js/controllers/home.controller.js
+++ www/js/controllers/home.controller.js
@@ -7,11 +7,11 @@ angular.module('home.controller', ['ui.swiper'])
     };
     $scope.slides = [
       {
-        img: "https://s14.postimg.org/m9s2e7ncv/h1_whatsapp.jpg",
+        img: "http://s14.postimg.org/m9s2e7ncv/h1_whatsapp.jpg",
         link: "http://www.personal.com.ar/tienda/prepago/whatsapp/?icn=prepago_whatsapp&ici=sin_gastar_datos_h1_home"
       },
       {
-        img: "https://s14.postimg.org/3wlllus1d/h1_internetentodo.jpg",
+        img: "http://s14.postimg.org/3wlllus1d/h1_internetentodo.jpg",
         link: "http://tienda.arnet.com.ar/detalle-arnet-6megas.html?icn=arnetyvoz&ici=oferta_h1_home"
       }];
 
