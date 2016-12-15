2e3481a7d29ee384b9154f7ddf9c0a87337f492e
Juan Brusco <juanbrusco_23@hotmail.com>
2016-12-15 12:55:45 -0300

    home + banner

--- www/js/controllers/home.controller.js
+++ www/js/controllers/home.controller.js
@@ -7,12 +7,37 @@ angular.module('home.controller', ['ui.swiper'])
     };
     $scope.slides = [
       {
-        img: "http://s14.postimg.org/m9s2e7ncv/h1_whatsapp.jpg",
-        link: "http://www.personal.com.ar/tienda/prepago/whatsapp/?icn=prepago_whatsapp&ici=sin_gastar_datos_h1_home"
+        img: "http://www.indiehoy.com/wp-content/uploads/2016/06/personal-fest-2016.jpg",
+        link: "http://www.personalfest.com.ar/#!/home"
       },
       {
-        img: "http://s14.postimg.org/3wlllus1d/h1_internetentodo.jpg",
-        link: "http://tienda.arnet.com.ar/detalle-arnet-6megas.html?icn=arnetyvoz&ici=oferta_h1_home"
+        img: "http://www.personalfest.com.ar/images/horarios.jpg",
+        link: "http://www.personalfest.com.ar/#!/home"
+      },
+      {
+        img: "http://www.personalfest.com.ar/images/mapa_predio.jpg",
+        link: "http://www.personalfest.com.ar/#!/home"
       }];
+
+    // $scope.options = {
+    //   loop: false,
+    //   effect: 'fade',
+    //   speed: 500,
+    // }
+    //
+    // $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
+    //   // data.slider is the instance of Swiper
+    //   $scope.slider = data.slider;
+    // });
+    //
+    // $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
+    //   console.log('Slide change is beginning');
+    // });
+    //
+    // $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+    //   // note: the indexes are 0-based
+    //   $scope.activeIndex = data.slider.activeIndex;
+    //   $scope.previousIndex = data.slider.previousIndex;
+    // });
 
   });
